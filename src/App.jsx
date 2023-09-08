816f3d7763963a419842c8cc60b6619e85a9a09e
Lautaro_Agustín_Bustos <lautaroabustos@gmail.com>
2023-09-08 15:54:10 -0300

    add slider and shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
+// App.jsx
 import { useEffect, useState } from "react";
 import "./App.css";
 import ProductCard from "./components/ProductCard";
 import SearchBar from "./components/SearchBar";
 import Logo from "./components/Logo";
 import Login from "./components/Login";
+import Slider from "./components/Slider";
+import AddProduct from "./components/AddProduct";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [searchText, setSearchText] = useState("");
+
   const handleChangeText = (e) => {
     setSearchText(e.target.value);
   };
+
+  const handleAddProduct = (product) => {
+    const newId = products.length ? products[products.length - 1].id + 1 : 1;
+    setProducts([...products, { id: newId, ...product }]);
+  };
+
+  const handleDeleteProduct = (id) => {
+    setProducts(products.filter((p) => p.id !== id));
+  };
+
+  const handleEditProduct = (id, updatedProduct) => {
+    setProducts(products.map(p => p.id === id ? { ...p, ...updatedProduct } : p));
+  };
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((prod) => setProducts(prod));
   }, []);
+
   return (
     <div>
       <header>
@@ -24,15 +43,15 @@ function App() {
         <Login />
       </header>
       <main>
-        {products
-          .filter((prod) =>
-            prod.title
-              .toLocaleLowerCase()
-              .includes(searchText.toLocaleLowerCase())
-          )
-          .map((p) => (
-            <ProductCard p={p} key={p.id} />
-          ))}
+        <Slider />
+        <AddProduct onAdd={handleAddProduct} />
+        <article className="products">
+          {products
+            .filter((prod) => prod.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
+            .map((p) => (
+              <ProductCard p={p} key={p.id} onDelete={handleDeleteProduct} onEdit={handleEditProduct} />
+            ))}
+        </article>
       </main>
     </div>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,27 @@
 import { MdExpandLess, MdExpandMore } from "react-icons/md";
+import EditProduct from "./EditProduct";
 import "./ProductCard.css";
 import { useState } from "react";
 
-function ProductCard({ p }) {
+function ProductCard({ p, onDelete, onEdit }) {
   const [isFullDescription, setIsFullDescription] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   const handleExpand = () => {
     setIsFullDescription(!isFullDescription);
   };
+
+  const handleSaveEdit = (id, updatedProduct) => {
+    onEdit(id, updatedProduct);
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return <EditProduct product={p} onSave={handleSaveEdit} onCancel={() => setIsEditing(false)} />;
+  }
+
   return (
-    <article>
+    <article className="products">
       <h3>{p.title}</h3>
       <div style={{ display: "flex" }}>
         <img src={p.image} alt={p.title} style={{ width: "50%" }} />
@@ -16,6 +29,8 @@ function ProductCard({ p }) {
           <p>{p.category}</p>
           <p>{p.price}</p>
           <button>add to cart</button>
+          <button onClick={() => setIsEditing(true)}>Editar</button>
+          <button onClick={() => onDelete(p.id)}>Eliminar</button>
         </div>
       </div>
       {isFullDescription ? (
